328e7504b5f4d257d158d49c45c940f2f11afb65
NGaastra <niels.gaastra@hotmail.com>
2022-10-24 19:55:38 +0200

    Added RFR to model trainer

--- training-api/resources/model_trainer.py
+++ training-api/resources/model_trainer.py
@@ -1,41 +1,39 @@
-# MLP for Pima Indians Dataset saved to single file
-# see https://machinelearningmastery.com/save-load-keras-deep-learning-models/
 import logging
 import os
 
-from flask import jsonify
-from keras.layers import Dense
-from keras.models import Sequential
+import pickle
 
+from flask import jsonify
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+from google.cloud import storage
 
 def train(dataset):
-    # split into input (X) and output (Y) variables
-    X = dataset[:, 0:8]
-    Y = dataset[:, 8]
-    # define model
-    model = Sequential()
-    model.add(Dense(12, input_dim=8, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    # compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # Fit the model
-    model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
+    X = dataset.drop(['MEDV'], axis=1)
+    y = dataset['MEDV']
+
+    model = RandomForestRegressor()
+    model.fit(X, y)
+
     # evaluate the model
-    scores = model.evaluate(X, Y, verbose=0)
+    accuracy = cross_val_score(model, X, y, cv=5, scoring='accuracy')
+    r2 = cross_val_score(model, X, y, cv=5, scoring='r2')
+
     text_out = {
-        "accuracy:": scores[1],
-        "loss": scores[0],
+        "accuracy:": accuracy,
+        "r2": r2,
     }
     logging.info(text_out)
     print(text_out)
     # Saving model in a given location provided as an env. variable
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
-        file_path = os.path.join(model_repo, "model.h5")
-        model.save(file_path)
+        file_path = os.path.join(model_repo, "model.sav")
+        pickle.dump(model, file_path)
         logging.info("Saved the model to the location : " + model_repo)
         return jsonify(text_out), 200
     else:
-        model.save("model.h5")
+        pickle.dumps(model)
         return jsonify({'message': 'The model was saved locally.'}), 200
